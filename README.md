d8e8d9c75cefb921ae8fbc271c8f414cf8f768b9
林煜展 <linyuzhan@linyuzhans-MacBook-Pro-9.local>
2025-06-24 00:18:27 +0800

    change some text

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# AutomaticCorrection
+# AutomaticCorrector
--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from automaticcorrection!")
+from pynput import keyboard
 
+def on_press(key):
+    try:
+        print(f'你按下了: {key.char}')
+    except AttributeError:
+        print(f'你按下了特殊鍵: {key}')
 
-if __name__ == "__main__":
-    main()
+def on_release(key):
+    if key == keyboard.Key.esc:
+        print('結束監聽')
+        return False  # 停止監聽
+    else:
+        print(f'你放開了: {key}')
+
+listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+listener.start()
+
+# 不會阻止其他程式使用鍵盤，這只是個監聽器
+listener.join()
+
